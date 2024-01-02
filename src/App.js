4b2fa34cefadd6a2303a5a43ce5e9087c7fc02b9
Gaganjot <gaganjothans777@gmail.com>
2024-01-02 00:19:27 -0500

    react-router-dom Added

--- src/App.js
+++ src/App.js
@@ -4,28 +4,53 @@ import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
 function App() {
-  const [mode, setMode] = useState('light');
+  const [mode, setMode] = useState("light");
 
-  const toggleMode = () =>{
-    if(mode === 'light'){
-      setMode('dark');
-      document.body.style.backgroundColor = 'grey';
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "grey";
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
     }
-    else{
-      setMode('light');
-      document.body.style.backgroundColor = 'white';
-    }
-
-  }
+  };
   return (
     <>
-      <Navbar title="Text Converter" about="About Us" mode={mode} toggleMode={toggleMode} />
-      {/* <Navbar /> */}
+    {/* <Router>
+      <Navbar
+        title="Text Converter"
+        about="About Us"
+        mode={mode}
+        toggleMode={toggleMode}
+      />
       <div className="container">
-        <TextForm mode ={mode} />
-        <About mode={mode} />
+        <Routes>
+          <Route path="/about">
+            <About mode={mode} />
+          </Route>
+          <Route path="/">
+            <TextForm mode={mode} />
+          </Route>
+        </Routes>
       </div>
+      </Router> */}
+      <Router>
+     
+      <Navbar
+        title="Text Converter"
+        about="About Us"
+        mode={mode}
+        toggleMode={toggleMode}
+      />
+        <Routes>
+          <Route exact path="/" element={<TextForm mode={mode} />}/>
+          <Route path="/about" element={<About mode={mode} />} />
+        </Routes>
+    </Router>
     </>
   );
 }
